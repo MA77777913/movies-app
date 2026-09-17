2982cefc68bfb33f7f70135a9a3d99f7992d2607
MA7-Mohamed-adel <aahhmmed.abdo.009@gmail.com>
2026-09-17 22:38:32 +0400

    edit-in-movie-screen

--- lib/features/movies/presentation/widgets/custom_bottom_navigation_bar.dart
+++ lib/features/movies/presentation/widgets/custom_bottom_navigation_bar.dart
@@ -3,7 +3,7 @@ import '../../../../core/utils/app_color.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   final int selectedIndex;
-  final Function(int) onTap;
+  final ValueChanged<int> onTap;
 
   const CustomBottomNavigationBar({
     super.key,
@@ -11,36 +11,68 @@ class CustomBottomNavigationBar extends StatelessWidget {
     required this.onTap,
   });
 
+  static const _items = <IconData>[
+    Icons.home_rounded,
+    Icons.search_rounded,
+    Icons.explore_outlined,
+    Icons.person_outline_rounded,
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-      height: 70,
-      decoration: BoxDecoration(
-        color: AppColor.gray,
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          _buildNavItem(Icons.home, 0),
-          _buildNavItem(Icons.search, 1),
-          _buildNavItem(Icons.explore, 2),
-          _buildNavItem(Icons.person, 3),
-        ],
+    return SafeArea(
+      top: false,
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+        height: 70,
+        decoration: BoxDecoration(
+          color: const Color(0xFF282A28),
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.4),
+              blurRadius: 16,
+              offset: const Offset(0, 6),
+            ),
+          ],
+        ),
+        child: Row(
+          children: List.generate(
+            _items.length,
+                (i) => Expanded(child: _buildNavItem(_items[i], i)),
+          ),
+        ),
       ),
     );
   }
 
   Widget _buildNavItem(IconData icon, int index) {
     final isSelected = selectedIndex == index;
-    return IconButton(
-      icon: Icon(
-        icon,
-        color: isSelected ? AppColor.yellow : Colors.white,
-        size: 30,
+
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () => onTap(index),
+        borderRadius: BorderRadius.circular(30),
+        child: Center(
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.easeOut,
+            padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+            decoration: BoxDecoration(
+              color: isSelected
+                  ? AppColor.yellow.withOpacity(0.15)
+                  : Colors.transparent,
+              borderRadius: BorderRadius.circular(22),
+            ),
+            child: Icon(
+              icon,
+              size: 28,
+              color: isSelected ? AppColor.yellow : AppColor.white,
+            ),
+          ),
+        ),
       ),
-      onPressed: () => onTap(index),
     );
   }
 }
